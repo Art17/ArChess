151ba36cdb8475de8ef5ebc03be8d9d329a62bc7
DESKTOP-06OJ8LI\Artem <ASaprykin97@gmail.com>
2016-12-09 21:13:44 +0200

    static root

--- archess/archess/settings.py
+++ archess/archess/settings.py
@@ -136,3 +136,8 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static/'),)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
